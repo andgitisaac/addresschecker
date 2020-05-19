d181d80961681771028e17698ea38b8320c89874
yangchris11 <yangchris11@gmail.com>
2020-05-19 11:54:37 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="addresschecker", 
+    version="0.0.1",
+    author="Hwai-Jin Peng, Cheng-Yen Yang",
+    author_email="",
+    description="A Python address spell checking package",
+    url="https://github.com/andgitisaac/addresschecker",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
